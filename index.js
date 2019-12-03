641214d6abe8e68d7b479bad91ee8688fbc18ef1
PalomaZelada <p.zelada.m@gmail.com>
2019-12-03 13:18:32 -0300

    Agregando funcion de verificacion de links

--- index.js
+++ index.js
@@ -1,51 +1,94 @@
-const fs = require("fs");
-const path = require("path");
-const FileHound = require('filehound');
-const Marked = require('marked');
+const fs = require('fs');
+const path = require('path');
+//const FileHound = require('filehound');
+const marked = require('marked');
+const fetch = require('fetch');
+const fetchUrl = fetch.fetchUrl;
 
-//Rescatando ruta
-let path2 = process.argv[2]
-//Convirtiendo en ruta absoluta
-path2 = path.normalize(path2)
-path2 = path.resolve(path2)
-//Para escribir los links
-let links = [];
+// Rescatando ruta
+let path2 = process.argv[2];
+// Convirtiendo en ruta absoluta
+path2 = path.normalize(path2);
+path2 = path.resolve(path2);
+// Para escribir los links
+const links = [];
 
-//Para leer archivo md
-const readMdFile = (path => {
-    return new Promise ((resolve, reject)=> {
-     fs.readFile(path, 'UTF-8',(error, archivo) => {
-        if(error){
-           reject(new Error("Existe un error en el archivo" + path2))
-        }
-        resolve(archivo)
-       });
-    })
+// Para leer archivo md
+// const readMdFile = (path => {
+//     return new Promise ((resolve, reject)=> {
+//      fs.readFile(path, 'UTF-8',(error, archivo) => {
+//         if(error){
+//            reject(new Error("Existe un error en el archivo" + path2))
+//         }
+//         resolve(archivo)
+//        });
+//     })
 
-})
+// })
 
-//Para extraer linkks
-const getLinks = readMdFile(path2)
-   getLinks.then(data => {
-    const renderer = new Marked.Renderer();
-    renderer.link = function(href, title, text){
+// Para extraer linkks
+function extractLinks(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'UTF-8', (error, data) => {
+      if (error) {
+        reject(new Error('No hay links'));
+      }
+      const renderer = new marked.Renderer();
+      renderer.link = function (href, file, text) {
         links.push({
-            href: href,
-            text: text,
-            file: path2,
+          href: href,
+          text: text,
+          file: path2,
+        });
+      };
+      marked(data, { renderer: renderer });
+      resolve(links);
+    });
+  });
+  return promise.then(
+    (links) => {
+      console.log(links);
+      let getLinks = links.map(function (links) {
+        return links.href;
+      });
+      return getLinks;
+    }
+  ).then(
+    (newLinks) => {
+      const statusPromises = newLinks.map(link => {
+        return linkStatus(link);
+      });
+      return Promise.all(statusPromises);
     })
+    // .then(
+    //   (statuses)=>{
+    //     console.log(statuses)
+    //   }
+    // )
 }
-    Marked(data, {renderer: renderer});
 
-    //return links;
-    console.log(links)
-})
-.catch(error => console.log("Hubo un error"))
+extractLinks(path2);
+
+const linkStatus = (url) => {
+  return new Promise((resolve) => {
+    fetchUrl(url, (error, meta, body) => {
+      if (meta) {
+        if (meta.status < 399) {
+          resolve(meta.status.toString())
+        } else (meta.status > 400); {
+          resolve(meta.status.toString());
+        }
+      }
+    })
+  })
+}
+
+// console.log(links)
 
 // console.log(links)
 
 // const a = readMdFile(path2);
-// a.then(console.log)        
+// a.then(console.log)
 
 
 // console.log(require.main === module);
--- mdlinks.js
+++ mdlinks.js
@@ -1,23 +1,90 @@
 const fs = require('fs');
 const path = require('path');
-const marked = require('marked')
+const marked = require('marked');
+const fetchUrl = require('fetch').fetchUrl;
 const FileHound = require('filehound');
 
-// let commandLine = process.argv[2]
-// console.log(commandLine)
+let pathMd = process.argv[2]
+pathMd = path.normalize(pathMd);
+pathMd = path.resolve(pathMd);
 
-// const findMd =(path =>{
-//         FileHound.create()
-//         .paths('/SCL011-MD-LINKS')
-//         .ext('md')
-//         .find()
-// files.then(console.log)
+const readMdFile = (path => {
+  return new Promise ((resolve, reject) => {
+    fs.readFile(path, 'utf-8',(error, file) => {
+      if(error){
+        reject(new Error("Hubo un error en el archivo " + pathMd));
+      };
+      resolve(file);
+    });
+  });
+});
+
+
+const mdLinks = (pathMd => {
+  const links = [];
+  return new Promise ((resolve, reject) => {
+    readMdFile(pathMd)
+    .then(res =>{
+      const renderer = new marked.Renderer();
+      renderer.link = function(href, title, text) {
+        links.push({
+          href: href,
+          text: text,
+          file: pathMd,
+        });
+      };
+      marked(res, { renderer: renderer });
+      console.log(links)
+      const statusProms = links.map(link => linkStatus(link.href));
+      Promise.all(statusProms)
+      .then(linksRes => {
+        resolve(linksRes)
+      });
+    })
+    .catch(err => {
+      reject(err)
+    } )
+  })
+})
+
+let foundLinks = mdLinks(pathMd).then(console.log)
+
+const linkStatus = (url) => {
+  return new Promise((resolve) => {
+    fetchUrl(url, (error, meta, body) => {
+      if (meta) {
+        if (meta.status === 200) {
+          resolve(meta.status.toString())
+        }else if (meta.status > 400); {
+          resolve(meta.status.toString())};
+        }else {
+          resolve("FAIL");
+        }
+          })
+  })
+}
+
+
+
+
+
+
+// const findLinks = readMdFile(pathMd)
+//    findLinks.then(data => {
+//     const renderer = new marked.Renderer();
+//     renderer.link = function(href, title, text){
+//         links.push({
+//             href: href,
+//             text: text,
+//             file: pathMd,
 //     })
+// }
+//     marked(data, {renderer: renderer});
 
-   
-    const files = FileHound.create()
-    .paths('/SCL011-MD-Links')
-    .ext('md')
-    .find();
-   
-  files.then(console.log);
+//     //return links;
+//     console.log(links)
+// })
+// .catch(error => console.log(error))
+
+//findLinks()
+
